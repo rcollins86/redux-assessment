9a144d239f8b0ec56ce56bc84efc950d531a69b9
Ryan Collins <40806147+rcollins86@users.noreply.github.com>
2023-12-06 10:54:33 -0600

    Add files via upload

---
+++ src/features/photos/photos.slice.js
@@ -0,0 +1,39 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { selectSearchTerm } from '../search/search.slice';
+import photos from './photos.data.js';
+
+const initialState = {
+  photos,
+};
+
+const options = {
+  name: 'photos',
+  initialState,
+  reducers: {
+    addPhoto: (state, action) => {
+      state.photos.unshift(action.payload);
+    },
+    removePhoto: (state, action) => {
+      state.photos.splice(
+        state.photos.findIndex((photo) => photo.id === action.payload),
+        1
+      );
+    },
+  },
+};
+
+const photosSlice = createSlice(options);
+
+export const { addPhoto, removePhoto } = photosSlice.actions;
+
+export default photosSlice.reducer;
+
+export const selectAllPhotos = (state) => state.photos.photos;
+export const selectFilteredPhotos = (state) => {
+  const photos = selectAllPhotos(state);
+  const searchTerm = selectSearchTerm(state);
+
+  return photos.filter((item) =>
+    item.caption.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+};
